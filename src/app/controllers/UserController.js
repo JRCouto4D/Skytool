d60ebab24e0d1575b856f0a647db81688321d105
unknown <jeffynho32@gmail.com>
2020-06-10 18:42:08 -0300

    update: controllers/User - Modificação nas condições de entradas de dados (sector)

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,7 +7,7 @@ class UserController {
       name: Yup.string().required(),
       email: Yup.string().email().required(),
       password: Yup.string().required().min(6),
-      sector: Yup.string().required(),
+      sector: Yup.string(),
     });
 
     if (!(await schema.isValid(req.body))) {
