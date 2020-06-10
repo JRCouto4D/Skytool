a4b7bc67a977900df72b2dd371ba4187d7d25503
unknown <jeffynho32@gmail.com>
2020-06-10 18:02:07 -0300

    add: controllers/User - add Feature store

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,35 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(6),
+      sector: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação de dados' });
+    }
+
+    const UserExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (UserExist) {
+      return res.status(400).json({ error: 'Usuário já cadastrado' });
+    }
+
+    const { id, name, email, provider, sector } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+      sector,
+    });
+  }
+}
+
+export default new UserController();
