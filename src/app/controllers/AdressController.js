daa39b2c1e7a5df7eba3075c2713c26eb383ce0e
unknown <jeffynho32@gmail.com>
2020-06-11 12:17:46 -0300

    add: controllers/adress - Feature store

--- src/app/controllers/AdressController.js
+++ src/app/controllers/AdressController.js
@@ -1,9 +1,42 @@
+import * as Yup from 'yup';
 import Address from '../models/Address';
 import User from '../models/User';
 
 class AddressController {
   async store(req, res) {
-    return res.json({ mensage: true });
+    const schema = Yup.object().shape({
+      user_id: Yup.number().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      neighborhood: Yup.string().required(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' });
+    }
+
+    const userExist = await User.findByPk(req.body.user_id);
+
+    if (!userExist) {
+      return res.status(400).json({ error: 'Usuário não registrado' });
+    }
+
+    const limitAdresses = await Address.findAll({
+      where: { user_id: req.body.user_id },
+    });
+
+    if (limitAdresses.length >= 3) {
+      return res
+        .status(400)
+        .json({ error: 'Exedido limite de registro de endereços' });
+    }
+
+    const address = await Address.create(req.body);
+
+    return res.json(address);
   }
 }
 
