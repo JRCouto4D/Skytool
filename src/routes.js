cc05bf7d441c8e1d4f334b7860bbabbdf1c0409d
unknown <jeffynho32@gmail.com>
2020-06-11 15:58:22 -0300

    add: routes - Incluindo rotas(update/delete) para addressController

--- src/routes.js
+++ src/routes.js
@@ -19,5 +19,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/users', UserController.store);
 
 routes.post('/adresses', AddressController.store);
+routes.put('/addresses/:id', AddressController.update);
+routes.delete('/addresses/:id', AddressController.delete);
 
 export default routes;
