24943c28bceba963e08743c843817c9d74512876
unknown <jeffynho32@gmail.com>
2020-06-11 16:02:09 -0300

    update: routes - Correção no nome da rota (update/delete) de adresses

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/users', UserController.store);
 
 routes.post('/adresses', AddressController.store);
-routes.put('/addresses/:id', AddressController.update);
-routes.delete('/addresses/:id', AddressController.delete);
+routes.put('/adresses/:id', AddressController.update);
+routes.delete('/adresses/:id', AddressController.delete);
 
 export default routes;
