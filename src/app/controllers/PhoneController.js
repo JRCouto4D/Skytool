c84a5b82f932de4b05a7a5e015e796162fb08a05
unknown <jeffynho32@gmail.com>
2020-06-12 19:58:15 -0300

    add: controllers/phone - Add feature store

--- src/app/controllers/PhoneController.js
+++ src/app/controllers/PhoneController.js
@@ -1,9 +1,22 @@
+import * as Yup from 'yup';
 import Phone from '../models/Phone';
 import User from '../models/User';
 
 class PhoneController {
   async store(req, res) {
-    return res.json({ mensage: true });
+    const schema = Yup.object().shape({
+      number: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos.' });
+    }
+
+    const dados = { id: req.userId, ...req.body };
+
+    const phone = await Phone.create(dados);
+
+    return res.json(phone);
   }
 
   async update(req, res) {
