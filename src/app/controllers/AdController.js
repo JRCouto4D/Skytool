b95fb2dd07b3eaa7b3328840a28c57b4b169a6a8
unknown <jeffynho32@gmail.com>
2020-06-12 23:10:42 -0300

    add: controllers/ad - add feature store

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -5,7 +5,21 @@ import File from '../models/File';
 
 class AdController {
   async store(req, res) {
-    return res.json({ mensage: true });
+    const schema = Yup.object().shape({
+      description: Yup.string().required(),
+      sector: Yup.string().required(),
+      status: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Dados inválidos' });
+    }
+
+    const dados = { user_id: req.userId, ...req.body };
+
+    const ads = await Ad.create(dados);
+
+    return res.json(ads);
   }
 
   async update(req, res) {
