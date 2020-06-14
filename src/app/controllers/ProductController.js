7f1686e617df7da1ff668a59d9cf029b8f971216
unknown <jeffynho32@gmail.com>
2020-06-14 13:33:53 -0300

    add: controller/product - add feature store

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,10 +1,32 @@
+import * as Yup from 'yup';
 import Product from '../models/Product';
 import User from '../models/User';
 import File from '../models/File';
 
 class ProductController {
   async store(req, res) {
-    return res.json({ mensage: true });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      description: Yup.string(),
+      value: Yup.number().required(),
+      sector: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' });
+    }
+
+    const user = await User.findByPk(req.userId);
+
+    if (!user.provider) {
+      return res.status(400).json({ error: 'O usuário deve ser um prestador' });
+    }
+
+    const dados = { user_id: req.userId, ...req.body };
+
+    const product = await Product.create(dados);
+
+    return res.json(product);
   }
 
   async update(req, res) {
