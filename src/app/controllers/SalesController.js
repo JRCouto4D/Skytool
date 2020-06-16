23f0bef14960c51b9838ca9b98f4a07cc217ac85
unknown <jeffynho32@gmail.com>
2020-06-16 08:48:55 -0300

    add: controller/sales - Add features store e delete

--- src/app/controllers/SalesController.js
+++ src/app/controllers/SalesController.js
@@ -4,7 +4,52 @@ import User from '../models/User';
 
 class SalesController {
   async store(req, res) {
-    return res.json({ mensage: true });
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' });
+    }
+
+    const { provider_id } = req.body;
+
+    if (provider_id === req.userId) {
+      return res.status(400).json({ error: 'Operação não autorizada.' });
+    }
+
+    const provider = await User.findByPk(provider_id);
+
+    if (!provider) {
+      return res.status(401).json({ error: 'Prestador não encontrado' });
+    }
+
+    if (!provider.provider) {
+      return res
+        .status(400)
+        .json({ error: 'Apenas prestadores de serviço podem realizar vendas' });
+    }
+
+    const checkSale = await Sales.findAll({
+      where: {
+        provider_id,
+        finished: null,
+      },
+    });
+
+    if (checkSale.length >= 1) {
+      return res
+        .status(400)
+        .json({ error: 'Existe uma venda não finalizada com este vendedor' });
+    }
+
+    const sale = await Sales.create({
+      user_id: req.userId,
+      provider_id,
+      payment: 'A VISTA',
+    });
+
+    return res.json(sale);
   }
 
   async update(req, res) {
@@ -12,7 +57,15 @@ class SalesController {
   }
 
   async delete(req, res) {
-    return res.json({ mensage: true });
+    const sale = await Sales.findByPk(req.params.id);
+
+    if (!sale) {
+      return res.status(400).json({ error: 'Venda não encontrada' });
+    }
+
+    await sale.destroy();
+
+    return res.send();
   }
 
   async show(req, res) {
