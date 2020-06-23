f94e2bd4da4be4f2854a1cbc1ee085016c9758f2
unknown <jeffynho32@gmail.com>
2020-06-23 12:55:04 -0300

    add: migration - Incluindo coluna address na tabela sales

---
+++ src/database/migrations/20200623142637-create-add_address_column_adresses_table.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('sales', 'address', {
+      type: Sequelize.INTEGER,
+      references: { model: 'adresses', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('sales', 'address');
+  },
+};
