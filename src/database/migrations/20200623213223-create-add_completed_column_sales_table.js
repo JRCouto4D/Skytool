34a1b01a02a6eaf87ba7a7627c6b3d22abf67fb4
unknown <jeffynho32@gmail.com>
2020-06-23 18:40:06 -0300

    add: migrations - Criando coluna completed_at na tabela sales

--- src/database/migrations/20200623213223-create-add_completed_column_sales_table.js
+++ src/database/migrations/20200623213223-create-add_completed_column_sales_table.js
@@ -1,23 +1,13 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn('sales', 'completed_at', {
+      type: Sequelize.DATE,
+      defaltValue: false,
+      allowNull: true,
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: (queryInterface) => {
+    return queryInterface.removeColumn('sales', 'completed_at');
+  },
 };
