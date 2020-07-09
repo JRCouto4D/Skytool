e2c0bdf0c275963507d36ffadad28f1db3f8e726
unknown <jeffynho32@gmail.com>
2020-07-09 17:17:16 -0300

    add: routes && Controllers/SearchProvider - Add feature que lista prestadores, de determinada categoria, pelo nome

---
+++ src/app/controllers/SearchProvider.js
@@ -0,0 +1,49 @@
+import { Op } from 'sequelize';
+import User from '../models/User';
+import File from '../models/File';
+
+class SearchProvider {
+  async show(req, res) {
+    const { name, page = 1 } = req.query;
+    const { sector } = req.params;
+
+    const total = await User.count({
+      where: {
+        provider: true,
+        sector: { [Op.iLike]: `${sector}` },
+        name: { [Op.iLike]: `${name}%` },
+      },
+    });
+
+    const providers = await User.findAll({
+      where: {
+        provider: true,
+        sector: { [Op.iLike]: `${sector}` },
+        name: { [Op.iLike]: `${name}%` },
+      },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+      limit: 6,
+      offset: (page - 1) * 6,
+      order: ['name'],
+      attributes: [
+        'id',
+        'name',
+        'email',
+        'sector',
+        'provider',
+        'open',
+        'evaluation',
+      ],
+    });
+
+    res.json({ providers, total });
+  }
+}
+
+export default new SearchProvider();
