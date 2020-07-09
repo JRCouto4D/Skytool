6681d6391c40ca00dd35f64f7ab1b084459d1ec7
unknown <jeffynho32@gmail.com>
2020-07-09 15:46:32 -0300

    add: Controller/Admin/Provider - Criando feature de cadastro de prestador

---
+++ src/app/controllers/Admin/ProviderController.js
@@ -0,0 +1,50 @@
+import * as Yup from 'yup';
+import User from '../../models/User';
+
+class ProviderController {
+  async store(req, res) {
+    const user = await User.findByPk(req.userId);
+
+    if (!user) {
+      return res.status(401).json({ error: 'Admin não encontrado' });
+    }
+
+    if (!user.admin) {
+      return res.status(401).json({ error: 'Este usuário não é um admin' });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+      sector: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação de dados' });
+    }
+
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExist) {
+      return res.status(400).json({ error: 'Prestador já cadastrado' });
+    }
+
+    const dados = {
+      ...req.body,
+      provider: true,
+    };
+
+    const { id, name, email, provider, sector } = await User.create(dados);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+      sector,
+    });
+  }
+}
+
+export default new ProviderController();
