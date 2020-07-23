8306f946b9008d906e7fdc426834ed3e417b1b5c
unknown <jeffynho32@gmail.com>
2020-07-23 13:59:02 -0300

    add: migration - add coluna category_id na tabela users

---
+++ src/database/migrations/20200723165653-add_categoryId_column_users_table.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'category_id', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.removeColumn('users', 'category_id');
+  }
+};
