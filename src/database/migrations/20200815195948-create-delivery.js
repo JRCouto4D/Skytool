84cb62bf54e4cd7ecd46c6dcbd063d5e5afed82c
unknown <jeffynho32@gmail.com>
2020-08-15 17:11:29 -0300

    env(migrations): Criando tabela delivery na base de dados

---
+++ src/database/migrations/20200815195948-create-delivery.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('delivery', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      mensage: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('delivery');
+  },
+};
