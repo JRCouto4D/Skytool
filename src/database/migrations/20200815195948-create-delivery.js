2282bf40ad9f34a087ac9c3e9e9c2f3f03d87fc3
unknown <jeffynho32@gmail.com>
2020-08-15 17:19:00 -0300

    env(migrations): Trocando o nome da tabela de 'delivery' para 'deliveries'

--- src/database/migrations/20200815195948-create-delivery.js
+++ src/database/migrations/20200815195948-create-delivery.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('delivery', {
+    return queryInterface.createTable('deliveries', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    return queryInterface.dropTable('delivery');
+    return queryInterface.dropTable('deliveries');
   },
 };
