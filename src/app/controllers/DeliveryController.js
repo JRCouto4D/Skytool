fd7253183a526af60c64a6bdde4a5ec1da7b4914
unknown <jeffynho32@gmail.com>
2020-08-15 23:54:49 -0300

    feat(DeliveryController): Incluindo metodo update aos controllers da tabela deliveries

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -40,6 +40,42 @@ class DeliveryController {
 
     return res.json(delivery);
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      price: Yup.number(),
+      mensage: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' });
+    }
+
+    const infoDelivery = await Delivery.findOne({
+      where: {
+        provider_id: req.userId,
+      },
+    });
+
+    if (infoDelivery.length <= 0) {
+      return res
+        .status(400)
+        .json({ error: 'Você não tem registros de informações de entrega' });
+    }
+
+    if (infoDelivery.length >= 1) {
+      if (infoDelivery.provider_id !== req.userId) {
+        return res.status(401).json({ error: 'Ação não autorizada' });
+      }
+    }
+
+    const { price, mensage } = await infoDelivery.update(req.body);
+
+    return res.json({
+      price,
+      mensage,
+    });
+  }
 }
 
 export default new DeliveryController();
