027281444f56e3a865a2052faf5caebc3746c081
unknown <jeffynho32@gmail.com>
2020-08-29 01:16:20 -0300

    feat(controllers/Orders/ListOrders): Incluindo informações da categoria a qual pertence o prestadorda serviço

--- src/app/controllers/Orders/ListOrders.js
+++ src/app/controllers/Orders/ListOrders.js
@@ -1,27 +1,35 @@
 import Sale from '../../models/Sale';
 import User from '../../models/User';
 import File from '../../models/File';
+import Category from '../../models/Category';
 
 class ListOrders {
   async index(req, res) {
+    const total = await Sale.count();
     const orders = await Sale.findAll({
       include: [
         {
           model: User,
           as: 'provider',
-          attributes: ['id', 'name', 'email'],
+          attributes: ['id', 'name'],
           include: [
             {
               model: File,
               as: 'avatar',
               attributes: ['name', 'path', 'url'],
             },
+            {
+              model: Category,
+              as: 'category',
+              attributes: ['name'],
+            },
           ],
         },
       ],
+      order: [['completed_at', 'DESC']],
     });
 
-    return res.json(orders);
+    return res.json({ orders, total });
   }
 
   async show(req, res) {
